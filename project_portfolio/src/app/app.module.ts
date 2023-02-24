7fc5cc31ba3cfc24c3ee2b7f69360853bbe98b38
Bhavitha Potlapally <bhavithapotlapally.13@gmail.com>
2023-02-24 11:52:26 -0600

    appts

--- project_portfolio/src/app/app.module.ts
+++ project_portfolio/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './middle/app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './middle/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,

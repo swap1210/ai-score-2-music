62978410c0aab6911c863058d1dc58c58d599615
Nidhi Gol <kinu.gol138@gmail.com>
2023-03-29 18:15:36 -0500

    Added features to load all Scores, Save changes to selected score, delete score and create a new blank score with multiple instruments

--- Project_Frontend/src/app/app.module.ts
+++ Project_Frontend/src/app/app.module.ts
@@ -1,50 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { SafeUrlPipe } from './common/safe-url.pipe';
-import { MessagesComponent } from './messages/messages.component';
-import { LoadingComponent } from './loading/loading.component';
-import { LoadingService } from './loading/loading.service';
-import { MessagesService } from './messages/messages.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./login/login.component";
+import { SafeUrlPipe } from "./common/safe-url.pipe";
+import { MessagesComponent } from "./messages/messages.component";
+import { LoadingComponent } from "./loading/loading.component";
+import { LoadingService } from "./loading/loading.service";
+import { MessagesService } from "./messages/messages.service";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { FlatioEditorComponent } from './main-home/flatio-editor/flatio-editor.component';
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { FlatioEditorComponent } from "./main-home/flatio-editor/flatio-editor.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatSelectModule } from "@angular/material/select";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		LoginComponent,
-		SafeUrlPipe,
-		MessagesComponent,
-		LoadingComponent,
-		FlatioEditorComponent,
-	],
-	imports: [
-		AppRoutingModule,
-		FormsModule,
-		BrowserModule,
-		BrowserAnimationsModule,
-		HttpClientModule,
-		ReactiveFormsModule,
-		RouterModule,
-		MatProgressSpinnerModule,
-		MatCardModule,
-		MatFormFieldModule,
-		MatButtonModule,
-		MatInputModule,
-	],
-	providers: [LoadingService, MessagesService],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SafeUrlPipe,
+    MessagesComponent,
+    LoadingComponent,
+    FlatioEditorComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatSnackBarModule,
+  ],
+  providers: [LoadingService, MessagesService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Project_Frontend/src/app/main-home/flatio-editor/flatio-editor.component.ts
+++ Project_Frontend/src/app/main-home/flatio-editor/flatio-editor.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { MatSelectChange } from "@angular/material/select";
+import { MatSnackBar } from "@angular/material/snack-bar";
 declare var Flat: any;
 
 @Component({
@@ -8,12 +11,53 @@ declare var Flat: any;
   styleUrls: ["./flatio-editor.component.scss"],
 })
 export class FlatioEditorComponent implements OnInit {
+  promptForm: FormGroup;
+  loading: boolean = false;
+  scores = [];
   scoreLoaded = false;
+  currentScore: any;
+  editor: any;
   @ViewChild("myInput")
   midiFile: ElementRef;
-  constructor(private http: HttpClient) {}
+  accessToken =
+    "ec419ca3c713761d7d3800ce79b0de2f4bde167fa76e85b8259057bfefb2fa2612dc260ef8a39b41c63fe74db1114f01e88391c2e0c31be3c98294de3a800bdb"; // Replace with your actual access token
 
-  ngOnInit() {}
+  modalOpen = false;
+  scoreTitle = "";
+  instruments = [
+    { group: "brass", instrument: "trumpet" },
+    { group: "keyboards", instrument: "piano" },
+    { group: "strings", instrument: "violin" },
+    { group: "strings", instrument: "violoncello" },
+    { group: "plucked-strings", instrument: "guitar-nylon" },
+    { group: "plucked-strings", instrument: "electric-bass" },
+    { group: "unpitched-percussion", instrument: "drumset-1" },
+  ];
+  selectedInstruments = {};
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private _snackBar: MatSnackBar
+  ) {
+    this.promptForm = this.fb.group({
+      title: [null, []],
+      selectedInstruments: [[]],
+    });
+  }
+
+  onInstrumentSelect(event: MatSelectChange): void {
+    this.promptForm.get("selectedInstruments").setValue(event.value);
+  }
+
+  onSubmit() {}
+
+  ngOnInit() {
+    this.getAllScores();
+  }
+
+  newEmptyScore() {
+    this.createEmptyScore(this.promptForm.get("title").value);
+  }
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
     if (file.type === "audio/midi") {
@@ -21,13 +65,79 @@ export class FlatioEditorComponent implements OnInit {
       reader.readAsDataURL(file);
       reader.onload = () => {
         const base64String = reader.result?.toString().split(",")[1];
-        console.log(base64String);
         this.midiFile.nativeElement.value = "";
         this.createScore(base64String);
       };
     } else {
       console.log("Please select a MIDI file.");
     }
+  }
+
+  deleteScore(score: any) {
+    this.loading = true;
+
+    const url = `https://api.flat.io/v2/scores/${score}?now=true`;
+
+    const headers = {
+      Authorization:
+        "Bearer ec419ca3c713761d7d3800ce79b0de2f4bde167fa76e85b8259057bfefb2fa2612dc260ef8a39b41c63fe74db1114f01e88391c2e0c31be3c98294de3a800bdb",
+      "Content-Type": "application/json",
+    };
+
+    this.http.delete(url, { headers }).subscribe(
+      (response: any) => {
+        this.loading = false;
+        this.getAllScores();
+      },
+      (error) => {
+        this.loading = false;
+        console.error("Error creating score:", error);
+      }
+    );
+  }
+
+  createEmptyScore(title) {
+    this.loading = true;
+    let score = {
+      title: title,
+      privacy: "public",
+      builderData: {
+        scoreData: {
+          instruments: this.instruments,
+        },
+        layoutData: {
+          notesSpacingCoeff: 2,
+          lengthUnit: "cm",
+          pageHeight: 29.7,
+          pageWidth: 21,
+          pageMarginTop: 1.5,
+          pageMarginBottom: 1.5,
+          pageMarginLeft: 2.5,
+          pageMarginRight: 2.5,
+        },
+      },
+    };
+
+    const url = "https://api.flat.io/v2/scores";
+
+    const headers = {
+      Authorization:
+        "Bearer ec419ca3c713761d7d3800ce79b0de2f4bde167fa76e85b8259057bfefb2fa2612dc260ef8a39b41c63fe74db1114f01e88391c2e0c31be3c98294de3a800bdb",
+      "Content-Type": "application/json",
+    };
+
+    this.http.post(url, score, { headers }).subscribe(
+      (response: any) => {
+        this.loading = false;
+        this.promptForm.get("title").setValue("");
+        this.loadScoreById(response.id);
+        this.getAllScores();
+      },
+      (error) => {
+        this.loading = false;
+        console.error("Error creating score:", error);
+      }
+    );
   }
 
   createScore(base64Data: string) {
@@ -48,14 +158,13 @@ export class FlatioEditorComponent implements OnInit {
 
     this.http.post(url, score, { headers }).subscribe(
       (response: any) => {
-        console.log("Score created:", response);
-        this.loadScore(response.id);
+        this.loadScoreById(response.id);
       },
       (error) => console.error("Error creating score:", error)
     );
   }
 
-  loadScore(scoreId: any) {
+  loadScoreById(scoreId: any) {
     this.scoreLoaded = true;
     var container = document.getElementById("embed-container");
     container.style.height = "500px";
@@ -68,5 +177,110 @@ export class FlatioEditorComponent implements OnInit {
         controlsPosition: "bottom",
       },
     });
+  }
+
+  loadScore(score: any) {
+    this.scoreLoaded = true;
+    document.getElementById("embed-container").innerHTML = "";
+    var container = document.getElementById("embed-container");
+    container.style.height = "500px";
+    this.currentScore = score.id;
+    this.editor = new Flat.Embed(container, {
+      score: score.id,
+      embedParams: {
+        appId: "63d202f3f3f8bdabcd8833e1",
+        sharingKey: score.sharingKey,
+        mode: "edit",
+        branding: false,
+        controlsPosition: "bottom",
+      },
+    });
+  }
+
+  exportFile(buffer, mimeType, ext) {
+    var blobUrl = window.URL.createObjectURL(
+      new Blob([buffer], {
+        type: mimeType,
+      })
+    );
+
+    var a = document.createElement("a");
+    a.href = blobUrl;
+    a.download = "exported-score." + ext;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
+
+  createNewRevision(data) {
+    var revisionData = {
+      data: data,
+      autosave: true,
+      description: "New revision",
+    };
+
+    fetch(`https://api.flat.io/v2/scores/${this.currentScore}/revisions`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(revisionData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this._snackBar.open("Changes Saved", "", {
+          duration: 3000,
+        });
+      })
+      .catch((error) => {
+        console.error(error); // this will log any errors to the console
+      });
+  }
+
+  saveChanges() {
+    var obj = this;
+    // this.editor.getMIDI().then(function (buffer) {
+    //   console.log("buffer", buffer);
+    //   obj.exportFile(buffer, "audio/midi", "mid");
+    // });
+
+    this.editor.getMusicXML().then(function (buffer) {
+      obj.createNewRevision(buffer);
+      // obj.exportFile(buffer, "application/vnd.recordare.musicxml+xml", "mxl");
+    });
+  }
+
+  getAllScores() {
+    const collectionId = "63d315c5cd4e47ddfc053a80"; // Replace with the actual ID of the collection you want to retrieve scores for
+
+    fetch(`https://api.flat.io/v2/collections/${collectionId}/scores`, {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("scores", data); // This will log the response data to the console
+        this.scores = data;
+      })
+      .catch((error) => {
+        console.error(error); // This will log any errors to the console
+      });
+
+    fetch("https://api.flat.io/v2/instruments", {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("instruments", data.instruments);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
